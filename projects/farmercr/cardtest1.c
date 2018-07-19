d3d8fa8a1f94436dc042413f7fd2746cdfa6eb89
farmercr <farmercr@users.noreply.github.com>
2018-07-18 21:54:42 -0400

    variable cleanup

--- projects/farmercr/cardtest1.c
+++ projects/farmercr/cardtest1.c
@@ -34,14 +34,14 @@ void assertResult(int expected, int actual, char* testDescription)
 
 int main()
 {
-	int newCards = 0;
-	int discarded = 1;
-	int xtraCoins = 0;
-	int shuffledCards = 0;
+	//int newCards = 0;
+	//int discarded = 1;
+	//int xtraCoins = 0;
+	//int shuffledCards = 0;
 
-	int i, j, m;
+	int i//, j, m;
 	int handpos = 0, choice1 = 0, choice2 = 0, choice3 = 0, bonus = 0;
-	int remove1, remove2;
+	//int remove1, remove2;
 	int seed = 50;
 	int numPlayers = 2;
 	int thisPlayer = 0;
