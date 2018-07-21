d4fe1b56fda161d942f4284d620eff22b394f465
farmercr <farmercr@users.noreply.github.com>
2018-07-20 22:05:59 -0400

    built base unittests to check makefile

--- projects/farmercr/unittest2.c
+++ projects/farmercr/unittest2.c
@@ -11,3 +11,40 @@
 *	https://www.gnu.org/software/make/manual/make.html
 ******************************************************************************/
 
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "rngs.h"
+
+#define TESTFUNCTION "Function Two"
+#define RED_TEXT "\x1b[31m"
+#define GREEN_TEXT "\x1b[32m"
+#define RESET_TEXT "\x1b[0m"
+
+void assertResult(int expected, int actual, char* testDescription)
+{
+	if (expected == actual)
+	{
+		printf(GREEN_TEXT "SUCCESS:" RESET_TEXT);
+		printf(" %s; Expected: %d, Actual: %d\n", testDescription, expected, actual);
+	}
+	else
+	{
+		printf(RED_TEXT "   FAIL:" RESET_TEXT);
+		printf(" %s; Expected: %d, Actual: %d\n", testDescription, expected, actual);
+	}
+}
+
+int main()
+{
+
+	printf("\n     ----- Testing %s Function -----\n", TESTFUNCTION);
+
+	assertResult(1, 1, "One equals one");
+	assertResult 2, 1, "One does not equal two");
+
+	printf("     ----- %s Unit Testing Complete -----\n\n", TESTFUNCTION);
+
+}
