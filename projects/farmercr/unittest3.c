d22db80498864af2f988c6a69dfb9740cfc12388
farmercr <farmercr@users.noreply.github.com>
2018-07-22 18:41:36 -0400

    updates to makefile and unittest files

--- projects/farmercr/unittest3.c
+++ projects/farmercr/unittest3.c
@@ -39,11 +39,65 @@ void assertResult(int expected, int actual, char* testDescription)
 
 int main()
 {
+	int seed = 50;
+	int numPlayers = 2;
+	int thisPlayer = 0;
+	struct gameState startGame, testGame;
+	int k[10] = { adventurer, baron, council_room, cutpurse, mine, minion,
+		remodel, smithy, tribute, village };
+	int expected = 0;
+	int actual = 0;
+
+	// initialize a game state and player cards
+	initializeGame(numPlayers, k, seed, &startGame);
 
 	printf("\n     ----- Testing %s Function -----\n", TESTFUNCTION);
 
-	assertResult(1, 1, "One equals one");
-	assertResult(2, 1, "One does not equal two");
+	int testCard = copper;
+	int deckCount = 0;
+	int handCount = 0;
+	int discardCount = 0;
+
+	expected = deckCount + handCount + discardCount;
+	actual = fullDeckCount(thisPlayer, testCard, &testGame);
+	assertResult(expected, actual, "No cards in any deck pile");
+
+	deckCount = 1;
+	testGame.deck[thisPlayer][0] = testCard;
+	testGame.deckCount[thisPlayer] = deckCount;
+	expected = deckCount + handCount + discardCount;
+	actual = fullDeckCount(thisPlayer, testCard, &testGame);
+	assertResult(expected, actual, "One copper in deck pile");
+
+	handCount = 1;
+	testGame.hand[thisPlayer][0] = testCard;
+	testGame.handCount[thisPlayer] = handCount;
+	expected = deckCount + handCount + discardCount;
+	actual = fullDeckCount(thisPlayer, testCard, &testGame);
+	assertResult(expected, actual, "One copper in deck and hand piles");
+
+	discardCount = 1;
+	testGame.discard[thisPlayer][0] = testCard;
+	testGame.discardCount[thisPlayer] = discardCount;
+	expected = deckCount + handCount + discardCount;
+	actual = fullDeckCount(thisPlayer, testCard, &testGame);
+	assertResult(expected, actual, "One copper in each pile");
+
+	expected = 0;
+	actual = fullDeckCount(thisPlayer, silver, &testGame);
+	assertResult(expected, actual, "No silver in any pile");
+
+	expected = 7;
+	actual = fullDeckCount(thisPlayer, copper, &startGame);
+	assertResult(expected, actual, "Regular start copper count");
+
+	expected = 3;
+	actual = fullDeckCount(thisPlayer, estate, &startGame);
+	assertResult(expected, actual, "Regular start estate count");
+
+	expected = 0;
+	actual = fullDeckCount(thisPlayer, silver, &startGame);
+	assertResult(expected, actual, "Regular start silver count");
 
 	printf("     ----- %s Unit Testing Complete -----\n\n", TESTFUNCTION);
 
