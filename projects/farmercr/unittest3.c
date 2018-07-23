03023538d06bd4e4425da48f6205db9a05e0a39e
farmercr <farmercr@users.noreply.github.com>
2018-07-22 22:05:36 -0400

    unittest3 first commit

--- projects/farmercr/unittest3.c
+++ projects/farmercr/unittest3.c
@@ -18,7 +18,7 @@
 #include <stdlib.h>
 #include "rngs.h"
 
-#define TESTFUNCTION "Function Three"
+#define TESTFUNCTION "numHandCards"
 #define RED_TEXT "\x1b[31m"
 #define GREEN_TEXT "\x1b[32m"
 #define RESET_TEXT "\x1b[0m"
@@ -39,65 +39,51 @@ void assertResult(int expected, int actual, char* testDescription)
 
 int main()
 {
-	int seed = 50;
-	int numPlayers = 2;
+	//int seed = 50;
+	//int numPlayers = 2;
 	int thisPlayer = 0;
-	struct gameState startGame, testGame;
-	int k[10] = { adventurer, baron, council_room, cutpurse, mine, minion,
-		remodel, smithy, tribute, village };
+	struct gameState testGame;
+	//int k[10] = { adventurer, baron, council_room, cutpurse, mine, minion,
+	//	remodel, smithy, tribute, village };
 	int expected = 0;
 	int actual = 0;
 
 	// initialize a game state and player cards
-	initializeGame(numPlayers, k, seed, &startGame);
+	//initializeGame(numPlayers, k, seed, &startGame);
 
 	printf("\n     ----- Testing %s Function -----\n", TESTFUNCTION);
 
-	int testCard = copper;
-	int deckCount = 0;
 	int handCount = 0;
-	int discardCount = 0;
+	
+	/*
+	int numHandCards(struct gameState *state) {
+		return state->handCount[whoseTurn(state)];
+	}
+*/
+	testGame.whoseTurn = thisPlayer;
 
-	expected = deckCount + handCount + discardCount;
-	actual = fullDeckCount(thisPlayer, testCard, &testGame);
-	assertResult(expected, actual, "No cards in any deck pile");
-
-	deckCount = 1;
-	testGame.deck[thisPlayer][0] = testCard;
-	testGame.deckCount[thisPlayer] = deckCount;
-	expected = deckCount + handCount + discardCount;
-	actual = fullDeckCount(thisPlayer, testCard, &testGame);
-	assertResult(expected, actual, "One copper in deck pile");
+	testGame.handCount[thisPlayer] = handCount;
+	expected = handCount;
+	actual = numHandCards(&testGame);
+	assertResult(expected, actual, "Empty hand");
 
 	handCount = 1;
-	testGame.hand[thisPlayer][0] = testCard;
 	testGame.handCount[thisPlayer] = handCount;
-	expected = deckCount + handCount + discardCount;
-	actual = fullDeckCount(thisPlayer, testCard, &testGame);
-	assertResult(expected, actual, "One copper in deck and hand piles");
+	expected = handCount;
+	actual = numHandCards(&testGame);
+	assertResult(expected, actual, "One card in hand");
 
-	discardCount = 1;
-	testGame.discard[thisPlayer][0] = testCard;
-	testGame.discardCount[thisPlayer] = discardCount;
-	expected = deckCount + handCount + discardCount;
-	actual = fullDeckCount(thisPlayer, testCard, &testGame);
-	assertResult(expected, actual, "One copper in each pile");
+	handCount = -1;
+	testGame.handCount[thisPlayer] = handCount;
+	expected = handCount;
+	actual = numHandCards(&testGame);
+	assertResult(expected, actual, "Negative one cards in hand");
 
-	expected = 0;
-	actual = fullDeckCount(thisPlayer, silver, &testGame);
-	assertResult(expected, actual, "No silver in any pile");
-
-	expected = 7;
-	actual = fullDeckCount(thisPlayer, copper, &startGame);
-	assertResult(expected, actual, "Regular start copper count");
-
-	expected = 3;
-	actual = fullDeckCount(thisPlayer, estate, &startGame);
-	assertResult(expected, actual, "Regular start estate count");
-
-	expected = 0;
-	actual = fullDeckCount(thisPlayer, silver, &startGame);
-	assertResult(expected, actual, "Regular start silver count");
+	handCount = 99;
+	testGame.handCount[thisPlayer] = handCount;
+	expected = handCount;
+	actual = numHandCards(&testGame);
+	assertResult(expected, actual, "Ninety-nine cards in hand");
 
 	printf("     ----- %s Unit Testing Complete -----\n\n", TESTFUNCTION);
 
