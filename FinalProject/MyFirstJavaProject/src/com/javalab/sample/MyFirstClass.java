0e1059881ce3de39df21e11d47264de9117feea5
farmercr <farmercr@users.noreply.github.com>
2018-08-04 18:58:30 -0400

    Final Project Addition with assignment 4 files

---
+++ FinalProject/MyFirstJavaProject/src/com/javalab/sample/MyFirstClass.java
@@ -0,0 +1,9 @@
+package com.javalab.sample;
+
+public class MyFirstClass {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Yes, I have begun!");
+	}
+}
---
+++ FinalProject/MyFirstJavaProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Craig
+ *
+ */
+module MyFirstJavaProject {
+}
